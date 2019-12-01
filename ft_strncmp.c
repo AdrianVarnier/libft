eb1fd7b2d37ae57ca1425088c913f9aaf93bf589
Adrian VARNIER <avarnier@e3r10p14.42.fr>
2019-12-01 11:43:30 +0100

    strncmp

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: avarnier <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/05 19:29:15 by avarnier          #+#    #+#             */
-/*   Updated: 2019/12/01 10:41:57 by avarnier         ###   ########.fr       */
+/*   Updated: 2019/12/01 11:42:15 by avarnier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,11 @@ int	ft_strncmp(const char *s1, const char *s2, size_t len)
 	size_t x;
 
 	x = 0;
-	while (x < len && s1[x] == s2[x] && s1[x] != '\0' && s2[x] != '\0')
+	while (x < len)
+	{
+		if (s1[x] != s2[x])
+			return ((unsigned char)s1[x] - (unsigned char)s2[x]);
 		x++;
-	return ((unsigned char)s1[x] - (unsigned char)s2[x]);
+	}
+	return (0);
 }
