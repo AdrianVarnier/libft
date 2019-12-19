242cccdac59a9ec81223f1157ec8fda688d5e051
Adrian VARNIER <avarnier@e3r11p14.42.fr>
2019-12-19 12:31:59 +0100

    strnstr

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,29 +6,29 @@
 /*   By: avarnier <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/08 15:31:15 by avarnier          #+#    #+#             */
-/*   Updated: 2019/12/01 10:44:58 by avarnier         ###   ########.fr       */
+/*   Updated: 2019/12/19 12:20:04 by avarnier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strnstr(const char *big, const char *little, size_t len)
+char	*ft_strnstr(const char *s, const char *find, size_t len)
 {
 	size_t i;
 	size_t j;
 
 	i = 0;
 	j = 0;
-	if (little[0] == '\0')
-		return ((char *)big);
-	while (big[i] != '\0' && i < len)
+	if (find[0] == '\0')
+		return ((char *)s);
+	while (s[i] != '\0' && i < len)
 	{
-		if (big[i] != little[j])
+		if (s[i] != find[j])
 			j = 0;
 		else
 			j++;
-		if (little[j] == '\0')
-			return ((char *)big + i - j + 1);
+		if (find[j] == '\0')
+			return ((char *)s + i - j + 1);
 		i++;
 	}
 	return (0);
