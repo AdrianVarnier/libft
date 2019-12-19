0c33c44dc8969eb5fec54217a490a7cebadc521c
Adrian VARNIER <avarnier@e3r11p14.42.fr>
2019-12-19 16:59:46 +0100

    part 1 finish

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: avarnier <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/08 15:31:15 by avarnier          #+#    #+#             */
-/*   Updated: 2019/12/19 12:20:04 by avarnier         ###   ########.fr       */
+/*   Updated: 2019/12/19 16:57:01 by avarnier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,22 +14,20 @@
 
 char	*ft_strnstr(const char *s, const char *find, size_t len)
 {
-	size_t i;
-	size_t j;
+	size_t	i;
+	size_t	findlen;
 
 	i = 0;
-	j = 0;
+	findlen = ft_strlen(find);
 	if (find[0] == '\0')
 		return ((char *)s);
-	while (s[i] != '\0' && i < len)
+	while (s[i] != '\0' && len >= findlen)
 	{
-		if (s[i] != find[j])
-			j = 0;
-		else
-			j++;
-		if (find[j] == '\0')
-			return ((char *)s + i - j + 1);
+		if (s[i] == find[0])
+			if (ft_strncmp(s + i, find, findlen) == 0)
+				return ((char *)s + i);
 		i++;
+		len--;
 	}
 	return (0);
 }
