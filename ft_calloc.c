53d3d6f98ed4bcc82770770e23743ac0089534c6
Adrian VARNIER <avarnier@e3r11p14.42.fr>
2019-12-26 10:30:33 +0100

    part 1 finish

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: avarnier <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/08 18:38:49 by avarnier          #+#    #+#             */
-/*   Updated: 2019/11/30 12:51:34 by avarnier         ###   ########.fr       */
+/*   Updated: 2019/12/26 10:26:41 by avarnier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,6 @@ void	*ft_calloc(size_t len, size_t octet)
 	}
 	if (!(s = (void *)malloc(len * octet)))
 		return (0);
-	ft_memset(s, 0, len * octet);
+	ft_bzero(s, len * octet);
 	return (s);
 }
--- ft_strdup.c
+++ ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: avarnier <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/14 11:11:52 by avarnier          #+#    #+#             */
-/*   Updated: 2019/11/14 13:40:38 by avarnier         ###   ########.fr       */
+/*   Updated: 2019/12/26 10:30:23 by avarnier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,16 +15,13 @@
 char	*ft_strdup(const char *s)
 {
 	int		i;
+	int		len;
 	char	*cpy;
 
 	i = 0;
-	if (!(cpy = (char *)malloc(sizeof(char) * (ft_strlen(s) + 1))))
+	len = ft_strlen(s) + 1;
+	if (!(cpy = (char *)malloc(sizeof(char) * len)))
 		return (0);
-	while (s[i] != '\0')
-	{
-		cpy[i] = s[i];
-		i++;
-	}
-	cpy[i] = '\0';
+	ft_memcpy(cpy, s, len);
 	return (cpy);
 }
